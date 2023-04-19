11337e759e2385b8c74880b15ee6d497828a20b7
Jefferson Davis <davisjd.jefferson23@gmail.com>
2023-04-19 12:25:56 +0300

    detecting winner from centre position. complete other centre from all corner

--- testing/Program.cs
+++ testing/Program.cs
@@ -1,4 +1,5 @@
-﻿
+﻿using System;
+
 class Player
 {
     private readonly char x_o;
@@ -49,6 +50,40 @@ class Board
         }
 
     }
+    private Boolean checkWin(int pos)
+    {
+        Console.WriteLine(pos);
+        char symbl = board[pos - 1];
+        int check = pos - 4;
+        int count = 0;
+        int match = 0;
+
+
+        while (count < 8)
+        {
+            if (count != 0 && count % 2 == 1)
+            {
+                Console.WriteLine("HERE");
+
+                check = (pos - 4) + (count / 2);
+                match = 0;
+            }
+            if (check - 1 > 0 && check - 1 < 9 && board[check - 1] == symbl)
+            {
+                ++match;
+                Console.WriteLine("check = {0} match = {1} symbl = {2}", check - 1, match, symbl);
+                if (match == 2)
+                    return true;
+            }
+            if (count == 0 || count % 2 == 0)
+            {
+                Console.WriteLine("inside");
+                check = (pos * 2) - check;
+            }
+            count++;
+        }
+        return false;
+    }
     public void makemove(Player a)
     {
         a.movePos();
@@ -58,8 +93,12 @@ class Board
             a.movePos();
         }
         board[a.pos - 1] = a.getX_O();
+        if (checkWin(a.pos))
+        {
+            Console.WriteLine("{0} wins.", a.getX_O());
+            System.Environment.Exit(0);
+        }
         increaseFilled();
-
     }
     public int getFilled()
     {
@@ -69,7 +108,6 @@ class Board
     {
         ++filled;
     }
-
 }
 
 class Program
@@ -85,6 +123,11 @@ class Program
         {
             Board.printBoard();
             bd.makemove(one);
+            if (bd.getFilled() == 9)
+            {
+                Console.WriteLine("Tie game.");
+                return;
+            }
             Board.printBoard();
             bd.makemove(two);
         }
